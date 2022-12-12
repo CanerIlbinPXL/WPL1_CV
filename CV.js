6807e28a9610f5e8fab4d0a6d73cb869b96e37e7
Caner Ilbin <caner.ilbin@student.pxl.be>
2022-12-12 15:53:52 +0100

    - lay out site gemaakt naar wens (div containers met de nodige inhoud, backgroundimg, alle assets toegevoegd) - zelfgemaakte assets toegevoegd (stars) - assets icons toegevoegd - javascript code implemented (on button click --> generate my script) - de huidige site opgemaakt in .css -  meta tag toegevoegd in header (device-scaling) wegens nieuwe leerstof lessen - alternerende opmaak oneven tabellen bij tabel diploma's (excluding the first table row) - foto hover (shadow) en click effect (border) toegevoegd - functionele footer toegevoegd - device scaling voor smaller en larger devices gecodeert

--- CV.js
+++ CV.js
@@ -1,5 +1,6 @@
 var text = 0;
-var actualtext = 'Ik ben Caner Ilbin, een 21-jarige student programmeren aan de Hogeschool PXL. Ik ben een gedreven en gemotiveerde programmeur die resultaatgericht en probleemoplossend te werk gaat. Naast programmeren hou ik mezelf ook graag in een goede fysieke vorm in de sportschool, daarnaast game ik ook graag.';
+var actualtext = 'Ik ben Caner Ilbin, een 21-jarige student programmeren aan de Hogeschool PXL. Ik ben een gedreven en gemotiveerde programmeur die resultaatgericht en probleemoplossend te werk gaat. Naast programmeren hou ik mezelf ook graag in een goede fysieke vorm in de sportschool, daarnaast game ik ook graag.' +
+    ' Ik wil mezelf naast programmeren ook verdiepen in het maken van designs en om die te animeren. Professional video editing spreekt mij ook enorm aan';
 var speed = 50;
 
 function AutoTypetxt() {
